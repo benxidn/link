9b94f563ba9d5cdf7d852c9c602598dcbe0a56de
Benx Official <107174900+benxidn@users.noreply.github.com>
2023-05-17 06:54:27 +0700

    Add files via upload

---
+++ my-link.js
@@ -0,0 +1,18 @@
+function Home() {
+   window.open("https://dexter.rf.gd","_self");
+   }
+function Channel() {
+   window.open("https://youtube.com/@DxtModzz","_self");
+   }
+function Unduh() {
+   window.open("https://youtu.be/8Eq_V1cTfHg","_self");
+   }
+function Drone() {
+   window.open("https://youtu.be/bKEHNU41Q4U","_self");
+   }
+function Fakecez() {
+   window.open("https://youtube.com/shorts/3QAvftFy6bU?feature=share","_self");
+   }
+function Dexter() {
+   window.open("https://youtube.com/@DxtModzz","_self");
+   }
