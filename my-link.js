55d61013b095fb7d67fee3d1b025e4d9a41f55d9
Benx Official <107174900+benxidn@users.noreply.github.com>
2023-06-13 01:38:19 +0700

    update my-link.js

--- my-link.js
+++ my-link.js
@@ -2,17 +2,17 @@ function Home() {
    window.open("https://dexter.rf.gd","_self");
    }
 function Channel() {
-   window.open("https://youtube.com/@DxtModzz","_self");
+   window.open("https://youtube.com/@DxtrModz","_self");
    }
 function Unduh() {
-   window.open("https://youtu.be/8Eq_V1cTfHg","_self");
+   window.open("https://youtu.be/3zdflmSSX4E","_self");
    }
 function Drone() {
-   window.open("https://youtu.be/bKEHNU41Q4U","_self");
+   window.open("https://youtu.be/snDGWmYdukc","_self");
    }
 function Fakecez() {
-   window.open("https://youtube.com/shorts/3QAvftFy6bU?feature=share","_self");
+   window.open("https://youtube.com/shorts/jE7b4AmOZWo?feature=share","_self");
    }
 function Dexter() {
-   window.open("https://youtube.com/@DxtModzz","_self");
+   window.open("https://youtube.com/@DxtrModz","_self");
    }
