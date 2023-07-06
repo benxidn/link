30d8f7177c29d417296f72aa52b792367eeecd93
Benx Official <107174900+benxidn@users.noreply.github.com>
2023-07-07 01:40:39 +0700

    update my-link.js

--- my-link.js
+++ my-link.js
@@ -2,17 +2,17 @@ function Home() {
    window.open("https://dexter.rf.gd","_self");
    }
 function Channel() {
-   window.open("https://youtube.com/@DxtModz01","_self");
+   window.open("https://youtube.com/@DxtMod","_self");
    }
 function Unduh() {
-   window.open("https://youtu.be/8wwo_lT6Ves","_self");
+   window.open("https://youtu.be/zXADxJkhTwM","_self");
    }
 function Drone() {
-   window.open("https://youtu.be/_95taeo59k8","_self");
+   window.open("https://youtu.be/o0uPkgp9FDg","_self");
    }
 function Fakecez() {
-   window.open("https://youtube.com/shorts/Jzeet7pnMtg?feature=share","_self");
+   window.open("https://youtube.com/shorts/agINltwisXo?feature=share","_self");
    }
 function Dexter() {
-   window.open("https://youtube.com/@DxtModz01","_self");
+   window.open("https://youtube.com/@DxtMod","_self");
    }
