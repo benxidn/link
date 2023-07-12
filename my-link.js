c38093b3cd371a80691103ca8fb91f02e1d711fe
Benx Official <107174900+benxidn@users.noreply.github.com>
2023-07-13 01:02:13 +0700

    update my-link.js

--- my-link.js
+++ my-link.js
@@ -5,13 +5,13 @@ function Channel() {
    window.open("https://youtube.com/@DxtrMoba","_self");
    }
 function Unduh() {
-   window.open("https://youtu.be/zXADxJkhTwM","_self");
+   window.open("https://youtu.be/eFFWkRrY3cs","_self");
    }
 function Drone() {
-   window.open("https://youtu.be/o0uPkgp9FDg","_self");
+   window.open("https://youtu.be/lDMRbxlpBI4","_self");
    }
 function Fakecez() {
-   window.open("https://youtube.com/shorts/agINltwisXo?feature=share","_self");
+   window.open("https://youtube.com/shorts/Nssk3XTeOtc?feature=share","_self");
    }
 function Dexter() {
    window.open("https://youtube.com/@DxtrMoba","_self");
