aaf3a4a2f3c21fd5d32a31008fb9f5474259a3fb
Benx Official <107174900+benxidn@users.noreply.github.com>
2024-06-16 17:14:32 +0700

    Update my-link.js

--- my-link.js
+++ my-link.js
@@ -2,7 +2,7 @@ function Home() {
    window.open("https://dexter.rf.gd","_self");
    }
 function Channel() {
-   window.open("https://youtube.com/@DexterAB","_self");
+   window.open("https://youtube.com/@DexterModzID","_self");
    }
 function Unduh() {
    window.open("https://youtu.be/f9Xum8Ps2mk","_self");
@@ -14,5 +14,5 @@ function Fakecez() {
    window.open("https://youtube.com/shorts/V9j53vs8stA?feature=share","_self");
    }
 function Dexter() {
-   window.open("https://youtube.com/@DexterAB","_self");
+   window.open("https://youtube.com/@DexterModzID","_self");
    }
